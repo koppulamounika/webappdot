8456d384e4df09c584f50d69456bb0530b47bfc9
koppula <mounica.koppula@gmail.com>
2021-05-01 15:47:09 +0530

    added

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "4b619f8d-1c19-4ae8-937e-e6409f11c5d1"
+  client_id       = "ef3a439d-e78d-4eb6-88d9-632d4261936d"
+  client_secret   = "XdYxT3vWJ0y9B.1c5.Zs6_wR3Q1gGDRa-q"
+  tenant_id       = "0b559b79-6cc6-43dd-b60f-aa3103ad738e"
+}
